e69ffe2f81e69448e53cea3d33879ec4b8150e5a
Charles Bittner-Rossmiller <charles.bittnerrossmiller@gmail.com>
2015-10-15 16:42:20 -0500

    user model

--- models/user.js
+++ models/user.js
@@ -1,1 +1,9 @@
 // User model w/ Mongoose
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    name: String,
+    password: String,
+});
