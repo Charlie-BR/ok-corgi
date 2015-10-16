53f0832be3532d4b90a8e693d9cac9c88ea1b2a0
Charles Bittner-Rossmiller <charles.bittnerrossmiller@gmail.com>
2015-10-16 10:14:34 -0500

    pseudocode likedDogs array

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,9 @@
 $(function() {
   console.log("Ruff ruff");
 });
+
+//create an array of 'likedDogs'
+
+//On button click, push (this.dogPic & this.dogName) into the array
+
+//++ on a counter for each time the specific URL is liked (count++)
